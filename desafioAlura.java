39784194ae88957b1a17782d2712007f452c39df
David Santos <davidmelo600@gmail.com>
2024-11-28 23:33:59 -0300

    up2

--- desafioAlura.java
+++ desafioAlura.java
@@ -5,47 +5,49 @@ public class desafioAlura {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
         String nomeDoCliente = "";
-        System.out.print("Boa noite, qual é o seu nome: ");
+        System.out.print("Boa noite! Seja bem-vindo(a)! Qual é o seu nome? ");
         nomeDoCliente = leitor.nextLine();
         int saldo = 0;
-        System.out.print("quanto que você quer depositar: ");
+        System.out.print("Que ótimo tê-lo(a) aqui, " + nomeDoCliente + "! Quanto você gostaria de depositar para começar? ");
         saldo = leitor.nextInt();
-        System.out.println("Dados iniciais do cliente\n");
+        System.out.println("\nDados iniciais do cliente\n");
         System.out.println("Nome do cliente: " + nomeDoCliente);
         System.out.println("Tipo de conta: Conta-Corrente");
-        System.out.println("Saldo atual: " + saldo +"\n");
-        int opçao = 0;
+        System.out.println("Saldo inicial: " + saldo + " reais\n");
+        int opcao = 0;
 
-        while (opçao != 4) {
-            System.out.println("Operações\n");
-            System.out.println("1- Consultar Saldo:");
-            System.out.println("2- Depositar valor");
-            System.out.println("3- Transferir valor");
-            System.out.println("4- Sair");
-            System.out.print("Digite a opção desejada: \n");
-            opçao = leitor.nextInt();
+        while (opcao != 4) {
+            System.out.println("Escolha uma das opções abaixo para continuar:\n");
+            System.out.println("1 - Consultar saldo");
+            System.out.println("2 - Depositar valor");
+            System.out.println("3 - Transferir valor");
+            System.out.println("4 - Sair");
+            System.out.print("Digite o número da opção desejada: ");
+            opcao = leitor.nextInt();
 
-            if (opçao == 1) {
-                System.out.println(" Saldo atual é: " + saldo +"\n");
-            } else if (opçao == 2) {
+            if (opcao == 1) {
+                System.out.println("\nSeu saldo atual é de: " + saldo + " reais.\n");
+            } else if (opcao == 2) {
                 float deposito = 0;
-                System.out.print("Digite quanto você quer depositar:");
+                System.out.print("\nDigite o valor que você deseja depositar: ");
                 deposito = leitor.nextFloat();
                 saldo += deposito;
-                System.out.println("Saldo atualizado é " + saldo + "\n");
-            } else if (opçao == 3) {
+                System.out.println("Depósito realizado com sucesso! Seu saldo agora é de " + saldo + " reais.\n");
+            } else if (opcao == 3) {
                 float transferir = 0;
-                System.out.print("Digite quanto você quer transferir do seu saldo: ");
+                System.out.print("\nDigite o valor que você deseja transferir: ");
                 transferir = leitor.nextFloat();
-                saldo -= transferir;
-                System.out.println("Saldo atual é: " + saldo + "\n");
                 if (transferir > saldo) {
-                    System.out.println("Dinheiro insuficiente para transferencia do valor\n");
+                    System.out.println("Saldo insuficiente para realizar a transferência. Tente um valor menor.\n");
+                } else {
+                    saldo -= transferir;
+                    System.out.println("Transferência concluída! Seu saldo atual é de: " + saldo + " reais.\n");
                 }
-            } else if (opçao == 4) {
+            } else if (opcao == 4) {
+                System.out.println("\nObrigado por utilizar nosso sistema, " + nomeDoCliente + "! Até logo!");
                 break;
             } else {
-                System.out.println("opção invalida. Digite umas da quatro opções\n");
+                System.out.println("\nOpção inválida. Por favor, escolha uma das opções disponíveis no menu.\n");
             }
         }
     }
